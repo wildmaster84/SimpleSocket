5a9288d37002f3e91d52e69b67abaf15459f7fce
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2024-07-12 00:01:04 -0400

    log the socket data

--- src/request.controller.js
+++ src/request.controller.js
@@ -7,8 +7,12 @@ class RequestController {
   }
   process() {
     //console.log("Raw Data (Hex):", this.data.toString("hex"));
+    // fsys\x80\x00\x00\x01\x00\x00\x00\xbe
+    // fsys\x80\x00\x00\x01\x00\x00\x00\xe7domainPartition.domain=eagames\nmessengerIp=10.10.10.113\nmessengerPort=13505\ndomainPartition.subDomain=NFS-2007\nTXN=Hello\nactivityTimeoutSecs=0\ncurTime="Nov-18-2023 03:58:33 UTC"\ntheaterIp=10.10.10.113\ntheaterPort=18195\x00
     console.log("Data: ", this.data.toString());
-      //this.socket.write(mergeBytes("fsys", Buffer.from([0xc0]), "\0\0\x01\0\0\0", Buffer.from([0xbb]), "TXN=Hello\0"));
+
+
+      this.socket.write(this.data);
     //this.socket.end();
   }
 }
