4a3c1cde4194dc7d47d4f5c4c4f51248f3f33aff
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2024-07-12 05:33:56 -0400

    added listener for theater server

--- src/main.js
+++ src/main.js
@@ -19,7 +19,26 @@ const server = net.createServer((socket) => {
     console.error("Socket error: " + err.message);
   });
 });
+const theater = net.createServer((socket) => {
+  // Handle incoming data
+  socket.on("data", (data) => {
+    const request = new RequestController(data, socket);
+    request.process();
+  });
 
+  // Handle client disconnection
+  socket.on("end", () => {
+    console.log("Client disconnected");
+  });
+
+  // Handle errors
+  socket.on("error", (err) => {
+    console.error("Socket error: " + err.message);
+  });
+});
+theater.listen(10135, () => {
+  console.log("Server listening on port 10135");
+});
 // Start the server on port 3000
 server.listen(10134, () => {
   console.log("Server listening on port 10134");
--- src/request.controller.js
+++ src/request.controller.js
@@ -11,28 +11,35 @@ class RequestController {
     // fsys\x80\x00\x00\x01\x00\x00\x00\xe7domainPartition.domain=eagames\nmessengerIp=10.10.10.113\nmessengerPort=13505\ndomainPartition.subDomain=NFS-2007\nTXN=Hello\nactivityTimeoutSecs=0\ncurTime="Nov-18-2023 03:58:33 UTC"\ntheaterIp=10.10.10.113\ntheaterPort=18195\x00
     console.log("Data: ", this.data.toString());
     let header = this.data.slice(0, 12);
-    let subDomain = this.data.toString().split("clientString=")[1].split("\x0a")[0];
-
-      this.socket.write(mergeBytes(
+    let subDomain = this.data
+      .toString()
+      .split("clientString=")[1]
+      .split("\x0a")[0];
+    let packet = this.data.slice(0, 4);
+    if (packet == "fsys") {
+      this.socket.write(
+        mergeBytes(
           // packet header
           header,
           // packet content
           `domainPartition.domain=eagames\n`,
-          `messengerIp=10.10.10.113\n`,
-          `messengerPort=13505\n`,
+          `messengerIp=68.46.244.148\n`,
+          `messengerPort=10135\n`,
           `domainPartition.subDomain=${subDomain}\n`,
           `TXN=Hello\n`,
           `activityTimeoutSecs=0\n`,
           `curTime="Nov-18-2023 03:58:33 UTC"\n`,
-          `theaterIp=10.10.10.113\n`,
-          `theaterPort=18195\0`
-      ));
+          `theaterIp=68.46.244.148\n`,
+          `theaterPort=10135\0`,
+        ),
+      );
+    }
     //this.socket.end();
   }
 }
 
 function mergeBytes(...arrays) {
-    const buffers = arrays.map((array) => Buffer.from(array));
-    return Buffer.concat(buffers);
+  const buffers = arrays.map((array) => Buffer.from(array));
+  return Buffer.concat(buffers);
 }
 module.exports = RequestController;
