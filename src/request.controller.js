def57a725012ed14060bbccb9134b40594fc8614
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2024-07-13 07:55:49 -0400

    fixed a typo

--- src/request.controller.js
+++ src/request.controller.js
@@ -27,14 +27,19 @@ class RequestController {
             Buffer.from([0x80, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0xfe]),
             // packet content
             `domainPartition.domain=eagames\x0a`,
-            `messengerIp=messaging.ea.com\x0a`,
-            `messengerPort=13505\x0a`,
+            `messengerIp=68.46.244.148\x0a`,
+            `messengerPort=10135\x0a`,
             `domainPartition.subDomain=beach\x0a`, // beach-360
             `TXN=Hello\x0a`,
             `activityTimeoutSecs=0\x0a`,
             `curTime="Feb-08-2010 17:49:40 UTC"\x0a`,
             `theaterIp=68.46.244.148\x0a`,
-            `theaterPort=10135\0`,
+            `theaterPort=10135\0`
+          ),
+        );
+        this.socket.write(
+          mergeBytes(
+            // packet header
             `fsys`,
             // source: http://old.zenhax.com/fesl-ea-com-protocol-t1282.html
             Buffer.from([0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3d]),
