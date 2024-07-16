ac2182c1993014128dcd0ec644340e005c57c55d
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2024-07-15 23:36:51 -0400

    fixed some packet stuff

--- src/packet.object.js
+++ src/packet.object.js
@@ -7,11 +7,7 @@ class Packet {
   create(header, data) {
     let length = data.toString().length + 12;
     console.log(length);
-    return mergeBytes(
-      header,
-      Buffer.from([intToHex(length)]),
-      data
-    );
+    return mergeBytes(header, Buffer.from([intToHex(length)]), data);
   }
 }
 
@@ -21,11 +17,11 @@ function mergeBytes(...arrays) {
 }
 
 function intToHex(num) {
-  if (typeof num !== 'number') {
-    throw new TypeError('Input must be a number');
+  if (typeof num !== "number") {
+    throw new TypeError("Input must be a number");
   }
   // Convert to hexadecimal string and pad with leading zeros if necessary
-  const hexString = num.toString(16).padStart(2, '0');
+  const hexString = num.toString(16).padStart(2, "0");
   // Prefix with '0x'
   return `0x${hexString}`;
 }
--- src/request.controller.js
+++ src/request.controller.js
@@ -13,55 +13,85 @@ class RequestController {
     console.log(this.data.toString());
 
     let action = this.data.toString().split("TXN=")[1].split("\x0a")[0];
-    let packet = this.data.slice(0, 4);
-    if (packet == "fsys") {
-      if (action == "Hello") {
-        let subDomain = this.data
-          .toString()
-          .split("clientString=")[1]
-          .split("-")[0]
-          .toUpperCase();
-        let packet1 = mergeBytes(
-          `domainPartition.domain=eagames\x0A`,
-          `messengerIp=68.46.244.148\x0A`,
-          `messengerPort=10135\x0A`,
-          `domainPartition.subDomain=${subDomain}\x0A`, // beach-360
-          `TXN=Hello\x0A`,
-          `activityTimeoutSecs=0\x0A`,
-          `curTime="${this.time()}"\x0A`,
-          `theaterIp=68.46.244.148\x0A`,
-          `theaterPort=10135\0`
-        );
-        let packet2 = mergeBytes(
-          `TXN=MemCheck\x0a`,
-          `memcheck.[]=0\x0a`,
-          `type=0\x0a`,
-          `salt=800225952\0`
-        );
-        this.socket.write(
-          new Packet(
-            mergeBytes(
-              // source: http://old.zenhax.com/fesl-ea-com-protocol-t1282.html
-              // Note: packet size is controlled by the header which is (header + body) | mister249
-              // the header MUST end with the packet size, in our case 230(0xE6)
-              `fsys`,
-              Buffer.from([0x80, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])
-            ),
-            packet1
-          )
-        );
+    let packet = this.data.toString().slice(0, 4);
+    switch (packet) {
+      case "fsys": {
+        if (action == "Hello") {
+          let subDomain = this.data
+            .toString()
+            .split("clientString=")[1]
+            .split("-")[0]
+            .toUpperCase();
+          let packet1 = mergeBytes(
+            `domainPartition.domain=eagames\x0A`,
+            `messengerIp=68.46.244.148\x0A`,
+            `messengerPort=10135\x0A`,
+            `domainPartition.subDomain=${subDomain}\x0A`, // beach-360
+            `TXN=Hello\x0A`,
+            `activityTimeoutSecs=0\x0A`,
+            `curTime="${this.time()}"\x0A`,
+            `theaterIp=68.46.244.148\x0A`,
+            `theaterPort=10135\0`
+          );
+          let packet2 = mergeBytes(
+            `TXN=MemCheck\x0a`,
+            `memcheck.[]=0\x0a`,
+            `type=0\x0a`,
+            `salt=800225952\0`
+          );
+          this.socket.write(
+            new Packet(
+              mergeBytes(
+                // source: http://old.zenhax.com/fesl-ea-com-protocol-t1282.html
+                // Note: packet size is controlled by the header which is (header + body) | mister249
+                // the header MUST end with the packet size, in our case 230(0xE6)
 
+                //           0x00 = ping packet
+                //           0x80 = single packet response
+                //           0xb0 = multi packet response
+                //           0xc0 = single packet request
+                //           0xf0 = multi packet request
+                `fsys`,
+                Buffer.from([0x80, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])
+              ),
+              packet1
+            )
+          );
+
+          this.socket.write(
+            new Packet(
+              mergeBytes(
+                // packet header
+                // 61(0x3D)
+                `fsys`,
+                Buffer.from([0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+              ),
+              packet2
+            )
+          );
+        }
+        break;
+      }
+      case "acct": {
+        // Todo: finish acct packet for xbox
+        let xuid = this.data.toString().split("xuid=")[1].split("\x0A")[0];
+        let packet = mergeBytes(
+          `TXN=NuXBL360Login\x0A`,
+          `xuid=${xuid}\x0A`,
+          `userId=000000001\x0A`,
+          `masterUserId=000000001\x0A`,
+          `userName=wildmaster84\0`
+        );
         this.socket.write(
           new Packet(
             mergeBytes(
-              // packet header
-              // 61(0x3D)
-              `fsys`,
-              Buffer.from([0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+              `acct`,
+              Buffer.from([0x80, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00])
             ),
-            packet2
+            packet
           )
         );
+        break;
       }
     }
     //this.socket.end();
